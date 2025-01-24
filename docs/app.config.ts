95fbaa191f038d2b2d465c05b852819bff70a690
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-01-24 23:05:51 +0400

    Init docs repository

---
+++ docs/app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  d0xigen: {
+    title: 'vueton',
+    description: 'TON integration for Vue 3',
+    url: 'https://vueton.d0rich.me',
+    author: 'Nikolai Dorofeev',
+    authorSocial: {
+      telegram: '@d0rich',
+      website: 'https://d0rich.me',
+      twitter: '@d0rich'
+    },
+    social: {
+      github: 'https://github.com/d0rich/vueton',
+    },
+    seo: {
+      keywords: ['web3', 'vue', 'ton', 'tonclient'],
+    },
+    features: {
+      gtag: {
+        id: 'G-ELNXV0GPLM'
+      }
+    },
+    d0richIndex: {
+      complexity: 4,
+      tags: ['web3', 'front-end']
+    }
+  }
+})
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  extends: ['d0xigen'],
+  compatibilityDate: '2024-12-02'
+})
