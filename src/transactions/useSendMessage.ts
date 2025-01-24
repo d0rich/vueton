b8683caf7abc716f29d55b67895d598ea1da4b85
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-01-24 22:17:44 +0400

    Enhance useSendMessage composable with detailed TypeScript types and JSDoc comments for improved clarity and usability

--- src/transactions/useSendMessage.ts
+++ src/transactions/useSendMessage.ts
@@ -1,26 +1,82 @@
 import { computed, ref } from 'vue'
+import type { Ref } from 'vue'
 
 import { useTonConnect } from '../tonconnect/useTonConnect'
 
 export interface SendMessageFnProps {
+  /** Address of the current user wallet as a string */
   userAddress: string
 }
 
 export interface UseSendMessageParams {
+  /**
+   * A function to send a message.
+   */
   sendMessageFn: (props: SendMessageFnProps) => Promise<void>
+  /**
+   * A function to validate if the message can be sent before actually doing it.
+   */
   preSendValidation?: () => Promise<boolean> | boolean
+  /**
+   * Hooks to be run on different stages of the message sending.
+   */
   hooks?: {
+    /**
+     * A hook to be run before the message is sent.
+     */
     onStart?: () => void
+    /**
+     * A hook to be run after the message is successfully sent.
+     * This hook will not be called if the message sending fails.
+     */
     onSuccess?: () => void
+    /**
+     * A hook to be run after the message sending fails.
+     */
     onFail?: () => void
   }
 }
 
-export function useSendMessage(props: UseSendMessageParams) {
+type Status = 'not_started' | 'in_progress' | 'success' | 'fail'
+
+export interface UseSendMessageResult {
+  /**
+   * Sends a message.
+   */
+  sendMessage: () => Promise<void>
+  /**
+   * The current status of the message sending.
+   */
+  status: Ref<Status>
+  /**
+   * Whether the message was successfully sent.
+   */
+  success: Ref<boolean>
+  /**
+   * Whether the message sending failed.
+   */
+  fail: Ref<boolean>
+  /**
+   * Whether the message sending is in progress.
+   */
+  isFetching: Ref<boolean>
+  /**
+   * Resets the message sending state.
+   */
+  reset: () => void
+}
+
+/**
+ * A high-level composable to send messages in TON Blockchain.
+ *
+ * @param props Parameters of the message sending.
+ * @returns Syncronous result of the message sending.
+ */
+export function useSendMessage(
+  props: UseSendMessageParams
+): UseSendMessageResult {
   const { tonAddress } = useTonConnect()
-  const status = ref<'not_started' | 'in_progress' | 'success' | 'fail'>(
-    'not_started'
-  )
+  const status = ref<Status>('not_started')
   const success = computed(() => status.value === 'success')
   const fail = computed(() => status.value === 'fail')
   const isFetching = computed(() => status.value === 'in_progress')
